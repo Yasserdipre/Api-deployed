0e9649715218157442c19061e28d1f888fe6e94d
Yasserdipre <yasserdipre09@gmail.com>
2023-08-13 20:15:41 +0200

    act 2.9

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ router.post("/contact", (req, res) => {
 });
 
 
-const PORT = process.env.PORT ?? 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
    console.log(`server listening on port http://localhost:${PORT}`);
